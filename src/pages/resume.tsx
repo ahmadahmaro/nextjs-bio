627aca3795b0ade6b7db24871f6f3a39b17ba714
aahmaro <ahmaro.ahmad@gmail.com>
2024-05-22 15:38:53 +0300

    split and differentiate my experience at Sakani

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -61,7 +61,10 @@ export default function Resume() {
               <td>
                 <Text color="blue">Backend</Text>
               </td>
-              <td>NodeJS, ExpressJS, NestJS, AWS RDS, PostgreSQL, DynamoDB, S3, MeiliSearch.</td>
+              <td>
+                NodeJS, ExpressJS, NestJS, AWS RDS, PostgreSQL, DynamoDB, S3,
+                MeiliSearch.
+              </td>
             </tr>
             <tr>
               <td>
@@ -95,61 +98,88 @@ export default function Resume() {
         <Title order={2}>Experience</Title>
         <Divider size="sm" />
         {/* Employer #1 */}
-        <Flex justify="space-between">
-          <Stack spacing={4}>
-            <Text color="blue">Sakani</Text>
+        <Stack spacing={0}>
+          <Flex justify="space-between" mb={16}>
+            <Text color="blue" fw="bold">
+              Sakani
+            </Text>
+            <Text fw="bold">Jerusalem</Text>
+          </Flex>
+          <Flex justify="space-between " mb={8}>
             <Text>Fullstack Developer</Text>
-          </Stack>
-          <Stack spacing={4} align="flex-end">
-            <Text>Jerusalem</Text>
-            <Text>March 2019 - Present</Text>
-          </Stack>
-        </Flex>
-        <List size="sm">
-          <List.Item>
-            Co-developed our web app using Next.js, Nest.js and Digital Ocean.
-          </List.Item>
-          <List.Item>
-            Implemented AuthN, AuthZ, database, API, and deployment.
-          </List.Item>
-          <List.Item>
-            Assembled a GIS team, that built over 500K+ shapes and street data
-            for the MENA region.
-          </List.Item>
-          <List.Item>
-            Implemented i18n for 2 languages in the Sakani codebase.
-          </List.Item>
-          <List.Item>
-            Built UI components ensuring, performance, responsiveness, and a11y
-            best practices.
-          </List.Item>
-        </List>
+            <Text>January 2021 - Present</Text>
+          </Flex>
+          <List size="sm" mb={16} withPadding>
+            <List.Item>
+              Co-developed our web app using Next.js, Nest.js and Digital Ocean.
+            </List.Item>
+            <List.Item>
+              Implemented AuthN, AuthZ, database, API, and deployment.
+            </List.Item>
+            <List.Item>
+              Assembled a GIS team, that built over 500K+ shapes and street data
+              for the MENA region.
+            </List.Item>
+            <List.Item>
+              Implemented i18n for 2 languages in the Sakani codebase.
+            </List.Item>
+            <List.Item>
+              Built UI components ensuring, performance, responsiveness, and
+              a11y best practices.
+            </List.Item>
+          </List>
+          <Flex justify="space-between" mb={8}>
+            <Text>Product Development Lead</Text>
+            <Text>March 2019 - December 2020</Text>
+          </Flex>
+
+          <List size="sm" mb={16} withPadding>
+            <List.Item>
+              Conducted market research to gather ideas and analyze competitors.
+            </List.Item>
+            <List.Item>
+              Assisted in developing business logic and processes for the
+              platform.
+            </List.Item>
+            <List.Item>
+              Contributed and advised optimal UX and UI practices to enhance
+              user experience.
+            </List.Item>
+            <List.Item>
+              Participated in creating mockups and wireframes to guide the
+              development team.
+            </List.Item>
+          </List>
+        </Stack>
         {/* Employer #2 */}
-        <Flex justify="space-between">
-          <Stack spacing={4}>
-            <Text color="blue">Rhino ERP</Text>
+
+        <Stack spacing={0}>
+          <Flex justify="space-between" mb={16}>
+            <Text color="blue" fw="bold">
+              Rhino ERP
+            </Text>
+            <Text fw="bold">Ramallah</Text>
+          </Flex>
+          <Flex justify="space-between" mb={8}>
             <Text>Frontend Developer</Text>
-          </Stack>
-          <Stack spacing={4} align="flex-end">
-            <Text>Ramallah</Text>
             <Text>April 2017 - Jan 2019 </Text>
-          </Stack>
-        </Flex>
-        <List size="sm">
-          <List.Item>
-            Worked as part of a team to establish UI library of consistent and
-            reusable ReactJS components.
-          </List.Item>
-          <List.Item>Managed application state using Redux.</List.Item>
-          <List.Item>
-            Contributed significantly to the design language for our internal UI
-            library.
-          </List.Item>
-          <List.Item>
-            Worked closely in creating our ERP core and UI with a backend
-            engineers and a graphic designers.
-          </List.Item>
-        </List>
+          </Flex>
+          <List size="sm" mb={8} withPadding>
+            <List.Item>
+              Worked as part of a team to establish UI library of consistent and
+              reusable ReactJS components.
+            </List.Item>
+            <List.Item>Managed application state using Redux.</List.Item>
+            <List.Item>
+              Contributed significantly to the design language for our internal
+              UI library.
+            </List.Item>
+            <List.Item>
+              Worked closely in creating our ERP core and UI with a backend
+              engineers and a graphic designers.
+            </List.Item>
+          </List>
+        </Stack>
         <Title order={2}>Education</Title>
         <Divider size="sm" />
         {/* Degree #1 */}
